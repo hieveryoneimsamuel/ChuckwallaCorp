a590629ecf8c605d6130ed17dabbbef41dbe02e7
Samuel Ch <112336467+hieveryoneimsamuel@users.noreply.github.com>
2023-03-27 17:54:14 -0600

    basic controller for milestone1

---
+++ company.php
@@ -0,0 +1,17 @@
+<?php
+
+class Controller_company extends Controller_Template
+{
+	
+	public function action_index()
+	{
+		die('index function in ourCompany.php');
+
+	}
+
+	public function action_about(){
+		die('about page');
+	}
+
+}
+?>
