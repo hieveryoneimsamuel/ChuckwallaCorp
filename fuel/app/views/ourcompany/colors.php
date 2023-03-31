2b39f1f9d2f7e6ac3e1cc838b6b98ca92a286971
Elena Wolvington <111819505+ProfessionalGerbil@users.noreply.github.com>
2023-03-30 20:00:47 -0600

    Add our colors view

---
+++ fuel/app/views/ourcompany/colors.php
@@ -0,0 +1,21 @@
+<body>
+    <h1>ChuckwallaCorp Colors page</h1>
+
+ 
+
+
+    
+    <form method='GET'> 
+        <p>Ask for num rows/cols here</p>
+        <input type="number" class="val" name="val1">
+        <button type="submit" name="rowsColsButton" id="btn-rowsCols"> Enter </button>
+
+        <p>Ask for num of colors here</p>
+        <input type="number" class="val" name="val2">
+        <button type="submit" name="numColsButton" id="btn-numColors"> Enter </button>
+    </form> 
+    
+    <br>
+    
+
+</body>
