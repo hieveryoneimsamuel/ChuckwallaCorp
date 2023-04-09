6d1d98ffeb098586c2b9ed4db7883fd57b7a7c27
Samuel Ch <112336467+hieveryoneimsamuel@users.noreply.github.com>
2023-04-09 17:31:55 -0600

    made checkForDuplicates()
    in Chuckwalla.js add a checkForDuplicates() function, which is getting called on each row in the color table. I think it is working...

--- local_html/m1/assets/js/chuckwalla.js
+++ local_html/m1/assets/js/chuckwalla.js
@@ -6,17 +6,50 @@ window.onload = function(){
 
     if(numRows !== null){
         console.log("numRows is: " + numRows);
-        printValue();
+        let rowsUnderConsideration = getRowIDs(numRows);
+        for(let i=0; i< rowsUnderConsideration.length; i++){
+            console.log("underConsideration: " + rowsUnderConsideration[i]);
+            checkForDuplicates(rowsUnderConsideration[i]);
+        }
+        
         
     }   
 
     
 }
 
+function getRowIDs(numRows){
+    let namesColors = ["RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "PURPLE", "GREY", "BROWN", "BLACK", "TEAL"];
+    let rowsWeGot = [];
+    for(let i=0; i< numRows; i++){
+        let tempName = "row" + namesColors[i];
+        rowsWeGot.push(tempName);
+    }
+    console.log(rowsWeGot);
+    return rowsWeGot;
+}
 
-function printValue(){
-    var redDD = document.getElementById('rowRED');
-    redDD.addEventListener('click',()=>{
-        console.log("First click");   
+
+function checkForDuplicates(idName){
+
+    var currRow = document.getElementById(idName);
+    currRow.addEventListener('click',()=>{
+        console.log("First click");
+        var initialVal = currRow.value;
+         
+
+        currRow.onchange = function() {
+            console.log("Second click");
+            console.log("newer value of redDD: " + currRow.value);
+
+            var othersVals = document.getElementsByClassName('mySelectElements');
+            for(let i=0; i< othersVals.length; i++){
+                if(othersVals[i].id !== idName && othersVals[i].value == currRow.value){
+                    window.alert("hey we've got a duplicate");
+                    currRow.value = initialVal;
+                }
+            }
+            
+        }
     });
 }
