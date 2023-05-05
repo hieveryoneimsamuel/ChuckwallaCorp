064f10a6d33a39439cd64ad2bcbae429249a8f52
Alex Glimco <alexpcg@rams.colostate.edu>
2023-05-05 10:51:52 -0600

    Changed all M1 to M2

--- fuel/app/views/companytemplate.php
+++ fuel/app/views/companytemplate.php
@@ -4,7 +4,7 @@
     </title>
     <meta charset="UTF-8">
     <meta name="author" content="Samuel Chamberlain, Elena Wolvington, Chloe Burke, Alex Glimco, Cassidy Roberts">
-    <meta name="description" content="Chuckwalla Corp M1">
+    <meta name="description" content="Chuckwalla Corp M2">
     
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -19,15 +19,15 @@
 <body>
     <header>
         <h1 id="header-content"> Chuckwalla <span style='color: #F19149;'>Corp</span></h1>
-        <img src="https://cs.colostate.edu:4444/~cburke10/m1/assets/img/ChuckwallaCorp.png" alt="Chuckwalla Logo">
+        <img src="https://cs.colostate.edu:4444/~cburke10/m2/assets/img/ChuckwallaCorp.png" alt="Chuckwalla Logo">
     </header>
 
     <div class="links">
         <nav>
             <ol>
-                <li><a href="https://cs.colostate.edu:4444/~stchambe/m1/index/company/index">HOME</a></li>
-                <li><a href="https://cs.colostate.edu:4444/~stchambe/m1/index/company/colors">COLORS</a></li>
-                <li><a href="https://cs.colostate.edu:4444/~stchambe/m1/index/company/about">ABOUT</a></li>
+                <li><a href="https://cs.colostate.edu:4444/~stchambe/m2/index/company/index">HOME</a></li>
+                <li><a href="https://cs.colostate.edu:4444/~stchambe/m2/index/company/colors">COLORS</a></li>
+                <li><a href="https://cs.colostate.edu:4444/~stchambe/m2/index/company/about">ABOUT</a></li>
                 <li><a href="https://en.wikipedia.org/wiki/Chuckwalla" alt="wikipedia page">CHUCKWALLA INFO</a></li>
             </ol> 
         </nav>
